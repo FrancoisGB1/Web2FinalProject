1870dcc4c35266a579e916bedbbf975cd02b6123
FrancoisGB1 <francoisguilletbeaulieu@gmail.com>
2025-12-01 18:36:48 -0500

    Added HP bar animations and pidgey respawn

---
+++ client/src/Attack.js
@@ -0,0 +1,61 @@
+
+export class Attack {
+
+    constructor(frames, settings = {}) {
+        this.frames = frames;
+        this.frameIndex = 0;
+        this.ticks = 0;
+        this.ticksPerFrame = settings.ticksPerFrame ?? 5; // how many updates between frames, kind of using a tertiary operator here
+        this.done = false;
+
+        const top = settings.top ?? "22%";
+        const right = settings.right ?? "18%";
+        const size = settings.size ?? 140;
+
+        // Create the div to show the animation
+        this.node = document.createElement("div");
+        Object.assign(this.node.style, {
+            position: "absolute",
+            width: size + "px",
+            height: size + "px",
+            top,
+            right,
+            backgroundImage: `url(${this.frames[0]})`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "contain",
+            pointerEvents: "none",
+            zIndex: "50",
+        });
+
+        // Append it
+        const container = document.getElementById("battle-wrapper");
+        if (!container) {
+            console.error("Attack: #battle-wrapper not found");
+            this.done = true;
+            return;
+        }
+        container.appendChild(this.node);
+    }
+
+    update() {
+        if (this.done) return;
+
+        this.ticks++;
+
+        // Change frame every X ticks
+        if (this.ticks % this.ticksPerFrame == 0) {
+            this.frameIndex++;
+
+            // remove after we're done
+            if (this.frameIndex >= this.frames.length) {
+                this.node.remove();
+                this.done = true;
+                return;
+            }
+
+            this.node.style.backgroundImage = `url(${this.frames[this.frameIndex]})`;
+        }
+    }
+}
+
+export default Attack;
--- client/src/page-chat.js
+++ client/src/page-chat.js
@@ -1,4 +1,6 @@
 import {registerCallbacks, sendMessage, signout, chatMessageLoop} from './chat-api';
+import Attack from "./Attack.js";
+
 
 window.addEventListener("load", () => {
     document.querySelector("textarea").onkeyup = function (evt) {
@@ -23,6 +25,181 @@ const memberListUpdate = members => {
 
 // MY CODE --------------------------------------------------------------------------------------------------------------------------------------------
 
+const scratchSfx = new Audio("audio/Scratch.mp3");
+const emberSfx   = new Audio("audio/Ember.mp3");
+let attackList = [];
+
+let enemyMaxHP = 35;
+let enemyHP = 35;
+const ENEMY_BAR_MAX_WIDTH = 45.6; // css width = 45.6%
+
+let enemyAlive = true;
+
+console.log(enemyHP)
+// Animations with sprite llist as param
+
+// Scratch
+function spawnScratchAttack() {
+    const frames = [
+        "img/battle/scratch1.png",
+        "img/battle/scratch2.png",
+        "img/battle/scratch3.png",
+        "img/battle/scratch4.png",
+    ];
+
+    const attack = new Attack(frames, {
+        top: "37%",
+        right: "20%",
+        size: 220,
+        ticksPerFrame: 15, // speed
+    });
+    
+    scratchSfx.volume = 0.3; 
+    scratchSfx.currentTime = 0; 
+    scratchSfx.play();
+    console.log(enemyHP);
+    if (!attack.done) {
+        attackList.push(attack);
+        damageEnemy(10)
+        updateEnemyHPMask()
+    }
+}
+
+
+// Ember
+
+function spawnEmberAttack() {
+    const frames = [
+        "img/battle/ember1.png",
+        "img/battle/ember2.png",
+        "img/battle/ember3.png",
+        "img/battle/ember4.png",
+        "img/battle/ember5.png",
+    ];
+
+    const attack = new Attack(frames, {
+        top: "37%",
+        right: "20%",
+        size: 220,
+        ticksPerFrame: 20,
+        
+    });
+
+    emberSfx.volume = 0.3; 
+    emberSfx.currentTime = 0; 
+    emberSfx.play();
+
+    if (!attack.done) {
+        attackList.push(attack);
+        damageEnemy(15)
+        updateEnemyHPMask()
+    }
+}
+
+// Change the width of the HP mask and reduce HP
+
+function damageEnemy(amount) {
+    if (!enemyAlive) return; // cant damage if dead
+
+    enemyHP = Math.max(0, enemyHP - amount);
+    updateEnemyHPMask();
+
+    if (enemyHP == 0) {
+        enemyFaint();
+    }
+}
+
+
+function updateEnemyHPMask() {
+    const bar = document.getElementById("enemy-hp-bar-area");
+    const hpPercent = enemyHP / enemyMaxHP;         // 1.0 = full, 0.7 = 70%
+    const lostPercent = 1 - hpPercent;             // 0.3 = 30% damage
+    const width = ENEMY_BAR_MAX_WIDTH * lostPercent;
+
+    bar.style.width = width + "%";
+}
+
+// fade out the pidgey
+function enemyFaint() {
+    enemyAlive = false;
+    console.log("Enemy Pidgey fainted!");
+
+    const sprite = document.getElementById("enemy-pokemon");
+
+    if (sprite) {
+        sprite.style.transition = "opacity 0.5s ease-out";
+        sprite.style.opacity = "0";
+    }
+
+    // spawn next
+    setTimeout(() => {
+        spawnNextEnemy();
+    }, 1500);
+}
+
+// reset values and restore sprite
+function spawnNextEnemy() {
+    console.log("A new enemy appears!");
+
+    enemyMaxHP = 35;
+    enemyHP = 35;
+    updateEnemyHPMask();
+    enemyAlive = true;
+
+    const sprite = document.getElementById("enemy-pokemon");
+    if (sprite) {
+        sprite.style.opacity = "1";
+        sprite.src = "img/battle/pidgey.png";  
+    }
+}
+
+
+// update attacks in list and splice when done
+
+function battleTick() {
+    // Update all active attacks
+    for (let i = attackList.length - 1; i >= 0; i--) {
+        const attack = attackList[i];
+        attack.update();
+        if (attack.done) {
+            attackList.splice(i, 1);
+        }
+    }
+
+    requestAnimationFrame(battleTick);
+}
+
+
+
+// button click events and launching animation frame loop
+
+window.addEventListener("load", () => {
+
+    // start the animation loop
+    updateEnemyHPMask()
+    battleTick();
+
+    const scratchBtn = document.querySelector(".move-btn1");
+    const emberBtn   = document.querySelector(".move-btn2");
+
+    if (scratchBtn) {
+        scratchBtn.addEventListener("click", () => {
+            spawnScratchAttack();
+        });
+    }
+
+    if (emberBtn) {
+        emberBtn.addEventListener("click", () => {
+            spawnEmberAttack();
+        });
+    }
+    
+});
+
+
+
+// Audio for the music and pidgey cry 
+
 const cryPidgey = new Audio("audio/pidgey.mp3");
 cryPidgey.volume = 0.3;
 
@@ -39,3 +216,5 @@ window.addEventListener("click", () => {
     }, 3500);
 
 }, { once: true });
+
+
